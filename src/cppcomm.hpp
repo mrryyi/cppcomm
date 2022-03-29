437c2364e5e7c239c229db624a4d3108e86c0ceb
unknown <jrhagelin@gmail.com>
2022-03-29 20:26:05 +0200

    Prettified code.

--- src/cppcomm.hpp
+++ src/cppcomm.hpp
@@ -247,12 +247,11 @@ private:
     return true;
   };
 
-  // Below code stolen from:
+  // valid_digit and is_valid_ip gotten from:
   // https://www.includehelp.com/c-programs/check-string-is-valid-ipv4-address-or-not.aspx
 
   /* return 1 if string contain only digits, else return 0 */
-  int valid_digit(char *ip_str)
-  {
+  int valid_digit(char *ip_str) {
     while (*ip_str) {
       if (*ip_str >= '0' && *ip_str <= '9')
         ++ip_str;
@@ -263,47 +262,46 @@ private:
     return 1;
   }
 
-  /* return 1 if IP string is valid, else return 0 */
-  int is_valid_ip(char *ip_str)
-{
-  int i, num, dots = 0;
-  char *ptr;
+    /* return 1 if IP string is valid, else return 0 */
+  int is_valid_ip(char *ip_str) {
+    int i, num, dots = 0;
+    char *ptr;
 
-  if (ip_str == NULL)
-    return 0;
-
-  const char * DELIM = ".";
-
-  ptr = strtok(ip_str, DELIM);
-
-  if (ptr == NULL)
-    return 0;
-
-  while (ptr) {
-
-    /* after parsing string, it must contain only digits */
-    if (!valid_digit(ptr))
+    if (ip_str == NULL)
       return 0;
 
-    num = atoi(ptr);
+    const char * DELIM = ".";
 
-    /* check for valid IP */
-    if (num >= 0 && num <= 255) {
-      /* parse remaining string */
-      ptr = strtok(NULL, DELIM);
-      if (ptr != NULL)
-          ++dots;
-    } else
+    ptr = strtok(ip_str, DELIM);
+
+    if (ptr == NULL)
       return 0;
 
+    while (ptr) {
+
+      /* after parsing string, it must contain only digits */
+      if (!valid_digit(ptr))
+        return 0;
+
+      num = atoi(ptr);
+
+      /* check for valid IP */
+      if (num >= 0 && num <= 255) {
+        /* parse remaining string */
+        ptr = strtok(NULL, DELIM);
+        if (ptr != NULL)
+            ++dots;
+      } else
+        return 0;
+
+    }
+
+    /* valid IP string must contain 3 dots */
+    if (dots != 3)
+      return 0;
+    
+    return 1;
   }
-
-  /* valid IP string must contain 3 dots */
-  if (dots != 3)
-    return 0;
-  
-  return 1;
-}
 }; // End Communicator
 
   
