ea402e652326b87dcae8506483da087dea4e0767
unknown <jrhagelin@gmail.com>
2022-04-02 19:45:48 +0200

    Removed tests for removed legacy-code

--- unit_tests/tests.cpp
+++ unit_tests/tests.cpp
@@ -10,7 +10,6 @@ struct TEST_DATA {
 TEST_DATA read_write_ByteBuffer_mixed_types();
 TEST_DATA read_write_ByteBuffer_strings();
 TEST_DATA read_write_ByteBuffer_any_data();
-TEST_DATA read_write_basic_uint8();
 void print_test(TEST_DATA test_data);
 
 int main (int args, const char* argv[]) {
@@ -22,7 +21,6 @@ int main (int args, const char* argv[]) {
     // So how about we just do expected behaviour first, and then we go into ways edge cases and ways to mess up?
 
     printf("Starting tests...\n");
-    print_test(read_write_basic_uint8());
     print_test(read_write_ByteBuffer_any_data());
     print_test(read_write_ByteBuffer_strings());
     print_test(read_write_ByteBuffer_mixed_types());
@@ -146,52 +144,6 @@ TEST_DATA read_write_ByteBuffer_any_data(){
     }
 
     return test_data;
-}
-
-TEST_DATA read_write_basic_uint8() {
-    TEST_DATA test_data;
-    test_data.success = false;
-    test_data.function_that_tests = __func__;
-    test_data.what_was_tested = "read/write basic uint8";
-    test_data.what_should_happen = "should write and read giving same values in order";
-
-    using namespace cppcomm;
-    // Prepare test-components
-    // -----------------------
-    uint8 buffer[3];
-    uint8* write_iterator = buffer;
-    // distinguishable values..
-    uint8 w_byte0 = 0x32;
-    uint8 w_byte1 = 0x61;
-    uint8 w_byte2 = 0x90;
-    
-    write_uint8(&write_iterator, w_byte0);
-    write_uint8(&write_iterator, w_byte1);
-    write_uint8(&write_iterator, w_byte2);
-
-    uint8* read_iterator = buffer;
-    // Bytes to read data to.
-    uint8 r_byte0;
-    uint8 r_byte1;
-    uint8 r_byte2;
-
-    read_uint8(&read_iterator, &r_byte0);
-    read_uint8(&read_iterator, &r_byte1);
-    read_uint8(&read_iterator, &r_byte2);
-
-    // Check if it went as expected.
-    if (   r_byte0 == w_byte0
-        && r_byte1 == w_byte1
-        && r_byte2 == w_byte2
-
-        // Negatory check as well for fun
-        && r_byte0 != w_byte1
-        && r_byte1 != w_byte2
-        && r_byte2 != w_byte0
-        )
-    { test_data.success = true; }
-
-    return test_data;    
 }
 
 void print_test(TEST_DATA test_data) {
